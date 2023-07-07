c5b871da063a85472da3639e7e74012c3bf8915c
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-07 20:34:47 +0300

    registration form

--- React-Native-hw-02-/App.js
+++ React-Native-hw-02-/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { RegistrationScreen } from './screens/RegistrationScreen';
 
 export default function App() {
--- React-Native-hw-02-/screens/RegistrationScreen.js
+++ React-Native-hw-02-/screens/RegistrationScreen.js
@@ -11,10 +11,11 @@ export function RegistrationScreen(){
 
             <Text style={styles.title}>Реєстрація</Text>
             <TextInput style={styles.input} placeholder='Логін'/>
-            <TextInput placeholder='Адреса електронної пошти'/>
-            <TextInput placeholder='Пароль'/>
-            <Button title="Зареєстуватися"/>
-            <Text>Вже є акаунт? Увійти</Text>
+            <TextInput style={styles.input} placeholder='Адреса електронної пошти'/>
+            <TextInput style={styles.input} placeholder='Пароль'/>
+            <Button title="Зареєстуватися" style={styles.btn_sign}/>
+            
+            <Text style={styles.sentense}>Вже є акаунт? Увійти</Text>
 
         </View>
     )
@@ -24,15 +25,34 @@ const styles = StyleSheet.create({
     title:{
         fontSize:30,
         color:'#212121',
+        marginTop:93,
+        marginBottom:30,
     },
     input:{
         height:50,
-        border:1,
-        backgroundColor:'#/BDBDBD',
+        width:"100%",
+        backgroundColor:'#F6F6F6',
         fontSize:16,
         paddingLeft:13,
+        borderWidth: 1,
+        borderColor: "1px solid #E8E8E8",
+        borderRadius: 6,
+        marginBottom:15,
+        color:'#BDBDBD ',
     },
     form:{
         padding:16,
+    },
+    btn_sign:{
+         
+        fontSize:16,
+        backgroundColor:"#FF6C00",
+        color:"#FFFFFF",
+        marginTop:40,  
+    },
+    sentense:{
+        color:"#1B4371",
+        fontSize:16,
+        fontWeight:400,
     }
   });
