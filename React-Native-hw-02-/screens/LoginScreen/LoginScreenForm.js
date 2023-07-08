1c740fc342c910524652404c2c3bed1b77bad924
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-08 16:10:05 +0300

    loginPage

--- React-Native-hw-02-/screens/LoginScreen/LoginScreenForm.js
+++ React-Native-hw-02-/screens/LoginScreen/LoginScreenForm.js
@@ -1,92 +1,116 @@
-import { Text, TextInput, View,StyleSheet,ImageBackground,TouchableOpacity,KeyboardAvoidingView } from "react-native";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  KeyboardAvoidingView,TouchableWithoutFeedback
+} from "react-native";
 
 import { useState } from "react";
 
-export function LoginScreen(){
-    const [isFocused, setIsFocused]=useState(false);
-    return(
-   
-         <View>
-         <ImageBackground style={styles.image} source={require('../Images/photoBG.png')}>
-            
-             <KeyboardAvoidingView   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-         <View style={{...styles.form}}>
-             <Text style={styles.title}>Увійти</Text>
-            
-             <TextInput style={styles.input} placeholder='Адреса електронної пошти' onFocus={()=>{setIsFocused(true)}}/>
-             <TextInput style={styles.input} placeholder='Пароль'onFocus={()=>{setIsFocused(true)}}/>
-             
-             <TouchableOpacity style={styles.btn_sign}>
-                 <Text style={styles.btn_sign_text}>Увійти</Text>
-             </TouchableOpacity>
-             
-             <Text style={styles.sentence}>Немає акаунту? Зареєструватися</Text>
-             
-         </View>
-         </KeyboardAvoidingView>
-         </ImageBackground>
-         </View>
+export function LoginScreen() {
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <View>
+      <ImageBackground
+        style={styles.image}
+        source={require("../Images/photoBG.png")}
+      >
+        <TouchableWithoutFeedback onPress={()=>{setIsFocused(false)}}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View style={{ ...styles.form,}}>
+            <Text style={styles.title}>Увійти</Text>
 
-    )
+            <TextInput
+              style={styles.input}
+              placeholder="Адреса електронної пошти"
+              onFocus={() => {
+                setIsFocused(true);
+              }}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Пароль"
+              onFocus={() => {
+                setIsFocused(true);
+              }}
+            />
+         <View style={{...styles.btnContainer, marginBottom:isFocused?0:143}}>
+            <TouchableOpacity style={styles.btn_sign}>
+              <Text style={styles.btn_sign_text}>Увійти</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.sentence}>Немає акаунту? Зареєструватися</Text>
+        </View>
+          </View>
+        </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </ImageBackground>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-  
-    title:{
-        fontSize:30,
-        color:'#212121',
-        marginTop:32,
-        marginBottom:30,
-    },
-    input:{
-        height:50,
-        width:"100%",
-        backgroundColor:'#F6F6F6',
-        fontSize:16,
-        paddingLeft:13,
-        borderWidth: 1,
-        borderColor: "#E8E8E8",
-        borderRadius: 6,
-        marginBottom:15,
-        color:'#BDBDBD',
-    },
-    form:{
-        padding:16,
-        // marginBottom:0,
-        marginTop:"80%", 
-        backgroundColor:"#FFFFFF",
-        borderTopLeftRadius:25,
-        borderTopRightRadius:25,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingBottom:143,
-        
-    },
-    btn_sign:{
-        fontSize:16,
-        backgroundColor:"#FF6C00",
-        color:"#FFFFFF",
-        marginTop:40, 
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius:100,
-        width:"100%",
-        height:51,
-        marginBottom:15,
-       
-    },
-    sentence:{
-        color:"#1B4371",
-        fontSize:16,
-        fontWeight:400,
-        
-    },
-    image:{
-       height:812,
-       width:375,
-    },
-    btn_sign_text:{
-        color:"#FFFFFF",
-        fontSize:16,
-    },
-  
-  });
+  title: {
+    fontSize: 30,
+    color: "#212121",
+    marginTop: 32,
+    marginBottom: 30,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#F6F6F6",
+    fontSize: 16,
+    paddingLeft: 13,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 6,
+    marginBottom: 15,
+    color: "#BDBDBD",
+  },
+  btnContainer:{
+    marginBottom:143,
+    width:"100%",
+    alignItems:"center",
+  },
+  form: {
+    padding: 16,
+    marginTop: "85%",
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+   
+    
+  },
+  btn_sign: {
+    fontSize: 16,
+    backgroundColor: "#FF6C00",
+    color: "#FFFFFF",
+    marginTop: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    width: "100%",
+    height: 51,
+    marginBottom: 15,
+  },
+  sentence: {
+    color: "#1B4371",
+    fontSize: 16,
+    fontWeight: 400,
+  },
+  image: {
+    height: 812,
+    width: 375,
+  },
+  btn_sign_text: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+});
